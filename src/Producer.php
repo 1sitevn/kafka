5a488eef5b6c952b1cfc75da821f47d8b22b23ca
TungNT <tungnt@gmail.com>
2020-06-04 11:01:17 +0700

    Test

--- src/Producer.php
+++ src/Producer.php
@@ -15,7 +15,7 @@ class Producer
     {
         $config = \Kafka\ProducerConfig::getInstance();
         $config->setMetadataRefreshIntervalMs(10000);
-        $config->setMetadataBrokerList('127.0.0.1:9082');
+        $config->setMetadataBrokerList('127.0.0.1:9092');
         $config->setBrokerVersion('1.0.0');
         $config->setRequiredAck(1);
         $config->setIsAsyn(false);
